7fdf907833932b905fc1558139b700461afa2699
kwabena.adu-darkwa <kwabenaadudarkwa@gmail.com>
2020-09-19 20:27:47 +0000

    main

--- dbConnection/db.js
+++ dbConnection/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.MONGO_URI
 // Connect to DB
-const conn = mongoose.connect("mongodb://localhost/Shokuapp")
+const conn = mongoose.connect(MONGO_URI)
  .then(() => console.log("db connected" ))
  .catch(err => console.log(err))
 
