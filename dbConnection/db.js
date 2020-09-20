243b09c868c062a7022b30039e1d491c12867857
kwabena.adu-darkwa <kwabenaadudarkwa@gmail.com>
2020-09-20 11:26:46 +0000

    useNewUrlParser: true

--- dbConnection/db.js
+++ dbConnection/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.MONGO_URI
 // Connect to DB
-const conn = mongoose.connect(mongoURI, { useUnifiedTopology: true })
+const conn = mongoose.connect(mongoURI, { useUnifiedTopology: true , useNewUrlParser: true,})
  .then(() => console.log("db connected" ))
  .catch(err => console.log(err))
 
