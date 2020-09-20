5dd3d0399753e1355be4bfab9334fab618413c6b
kwabena.adu-darkwa <kwabenaadudarkwa@gmail.com>
2020-09-20 11:45:56 +0000

    maon

--- dbConnection/db.js
+++ dbConnection/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.MONGO_URI
 // Connect to DB
-const conn = mongoose.connect("mongodb+srv://kwabs:kad1234@cluster0.pykji.mongodb.net/shoku?retryWrites=true&w=majority", { useUnifiedTopology: true , useNewUrlParser: true,})
+const conn = mongoose.connect(mongoURI, { useUnifiedTopology: true , useNewUrlParser: true,})
  .then(() => console.log("db connected" ))
  .catch(err => console.log(err))
 
